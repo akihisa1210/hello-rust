1b12c610cf711cec33ce2231169b524e8d0ed611
akihisa1210 <19635263+akihisa1210@users.noreply.github.com>
2023-08-26 11:28:59 +0900

    chore: rust-book chapter7

---
+++ rust-book/chapter7/7-4/restaurant/src/lib.rs
@@ -0,0 +1,22 @@
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+    }
+}
+
+pub use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
+}
+
+use std::fmt::Result;
+use std::io::Result as IoResult;
+
+fn function1() -> Result {
+    Ok(())
+}
+
+fn function2() -> IoResult<()> {
+    Ok(())
+}
